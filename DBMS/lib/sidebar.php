e7a7debbb6b1727d065dcb7d8c208f11be4a2c22
Tanushree Borase <tanushreebcs@gmail.com>
2025-10-08 13:12:27 +0530

    Add files via upload

---
+++ DBMS/lib/sidebar.php
@@ -0,0 +1,26 @@
+			<div class="life-side-bar">
+			<div class="hero-container">                  
+			
+					
+			</div>
+			
+			
+		
+					
+					
+							<ul class="nav nav-tabs nav-stacked">
+						<li class="">
+						<a   href="#">&nbsp;    Contact US</a>
+						</li>
+					</ul>
+<strong>Address</strong>
+<p>SRM University, Chennai - 603202, Tamil Nadu, India </p>
+<p>Telephone No. :</p>
+<p>0542-2361727</p>
+
+
+
+			
+			</div>
+			
+	
